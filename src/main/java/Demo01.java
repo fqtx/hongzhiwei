65c9922ecfaaff6fd820c047506ad2740fa9c59f
zoulingchao@126.com <x110y911>
2017-11-06 11:33:10 +0800

    Initial commit

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Demo01 {
+    //JUtil5
+    @Test
+    public void test01(){
+        List list = new ArrayList();
+        Map map = new LinkedHashMap();
+        map.put("a",1);
+        map.put("b",2);
+        list.add(map);
+        //fastjson
+        String json = JSON.toJSONString(list);
+        System.out.println(json);
+    }
+}
